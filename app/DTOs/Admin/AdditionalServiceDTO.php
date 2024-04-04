9cb209e53e752f7d508f7914183ceef91224d833
cekator3 <hamsterdreams@inbox.ru>
2024-04-04 12:06:21 +0500

    documented additionalservicedto class

--- app/DTOs/Admin/AdditionalServiceDTO.php
+++ app/DTOs/Admin/AdditionalServiceDTO.php
@@ -3,7 +3,7 @@
 namespace App\DTOs\Admin\AdditionalServices;
 
 /**
- *
+ * A subsystem for reading application data about additional services
  */
 class AdditionalServiceDTO
 {
@@ -21,32 +21,48 @@ class AdditionalServiceDTO
         $this->previewImageFilename = $previewImageFilename;
     }
 
+    /**
+     * Returns the name of the additional service
+     */
     public function getName() : string
     {
         assert($this->name !== '', 'accessing not initialized property: $name');
         return $this->name;
     }
 
+    /**
+     * Returns the description of the additional service
+     */
     public function getDescription() : string
     {
         assert($this->description !== '', 'accessing not initialized property: $description');
         return $this->description;
     }
 
+    /**
+     * Returns the preview image filename of the additional service
+     */
     public function getPreviewImageFilename() : string
     {
         assert($this->previewImageFilename !== '', 'accessing not initialized property: $previewImageFilename');
         return $this->previewImageFilename;
     }
 
+    /**
+     * Returns the preview image URL of the additional service
+     */
     public function getPreviewImageUrl() : string
     {
         assert($this->previewImageUrl !== '', 'accessing not initialized property: $previewImageUrl');
         return $this->previewImageUrl;
     }
 
+    /**
+     * Sets the URL of the preview image for the additional service.
+     */
     public function setPreviewImageUrl(string $previewImageUrl) : void
     {
+        assert($this->previewImageUrl === '', 'Trying to replace the URL: $previewImageUrl');
         $this->previewImageUrl = $previewImageUrl;
     }
 }
