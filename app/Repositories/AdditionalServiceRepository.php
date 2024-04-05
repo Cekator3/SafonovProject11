158605eb9a2c32fdd9b675d2a4902a80d038c161
cekator3 <hamsterdreams@inbox.ru>
2024-04-06 00:54:45 +0500

    Created documentation for new repository: AdditionalServiceRepository

--- app/Repositories/AdditionalServiceRepository.php
+++ app/Repositories/AdditionalServiceRepository.php
@@ -2,10 +2,65 @@
 
 namespace App\Repositories;
 
+use App\DTOs\Admin\AdditionalServiceDTO;
+
 /**
  * Subsystem for interaction with stored information on additional services
  */
 class AdditionalServiceRepository
 {
-    
+    /**
+     * Returns all additional services.
+     * @return AdditionalServiceDTO[]
+     */
+    public function getAll() : array
+    {
+        // ...
+    }
+
+    /**
+     * Returns additional service.
+     */
+    public function get(int $id) : AdditionalServiceDTO|null
+    {
+        // ...
+    }
+
+    /**
+     * Adds additional service.
+     *
+     * @param AdditionalServiceCreationViewModel $additionalService
+     * New additional service's data.
+     * @param AdditionalServiceCreationErrors $errors
+     * An object for storing operation errors.
+     */
+    public function add(AdditionalServiceCreationViewModel $additionalService,
+                        AdditionalServiceCreationErrors $errors) : void
+    {
+
+    }
+
+    /**
+     * Updates additional service.
+     *
+     * @param AdditionalServiceUpdateViewModel $additionalService New additional service's data.
+     * @param AdditionalServiceCreationErrors $errors
+     * An object for storing operation errors.
+     */
+    public function update(AdditionalServiceUpdateViewModel $additionalService,
+                           AdditionalServiceUpdateErrors $errors) : void
+    {
+        // ...
+    }
+
+    /**
+     * Deletes additional service.
+     *
+     * @param int $id Identifier of the additional service
+     * @param bool &$isSuccess Will be set to true if operation was successful
+     */
+    public function remove(int $id, bool &$isSuccess) : void
+    {
+        // ...
+    }
 }
