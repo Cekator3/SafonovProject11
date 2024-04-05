023e0db9c86d5db538d7f0d8c170e974988e1196
cekator3 <hamsterdreams@inbox.ru>
2024-04-06 01:15:17 +0500

    created implementation for controller

--- app/Http/Controllers/Admin/AdditionalServices/ListAdditionalServicesController.php
+++ app/Http/Controllers/Admin/AdditionalServices/ListAdditionalServicesController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Admin\AdditionalServices;
 
+use App\Repositories\AdditionalServiceRepository;
 use Illuminate\View\View;
 
 class ListAdditionalServicesController
 {
     public function showAdditionalServices() : View
     {
-        // ...
+        $additionalServices = new AdditionalServiceRepository();
+
         return view('admin.additional-services.list',
-                    ['additionalServices' => $this->getTestData()]);
+                    ['additionalServices' => $additionalServices->getAll()]);
     }
 }
