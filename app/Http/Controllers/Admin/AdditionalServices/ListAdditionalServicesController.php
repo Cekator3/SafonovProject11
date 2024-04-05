46ff2d87cac55a7b4432cf837da26a980d263c9b
cekator3 <hamsterdreams@inbox.ru>
2024-04-05 17:56:05 +0500

    small fix

--- app/Http/Controllers/Admin/AdditionalServices/ListAdditionalServicesController.php
+++ app/Http/Controllers/Admin/AdditionalServices/ListAdditionalServicesController.php
@@ -11,9 +11,17 @@ class ListAdditionalServicesController
     {
         $arr = [];
 
-        for ($i=0; $i < 15; $i++)
+        $service = new AdditionalServiceDTO(0, "test0", 'asdf', '');
+        $service->setPreviewImageUrl('/assets/images/test.gif');
+        $arr []= $service;
+
+        $service = new AdditionalServiceDTO(0, "test0", 'Aliquip dolor qui id qui adipisicing incididunt nulla fugiat in elit proident dolor mollit.Nisi quis exercitation deserunt laboris laborum deserunt occaecat reprehenderit pariatur nostrud commodo eiusmod qui reprehenderit.Non excepteur magna ullamco magna mollit amet.Aliqua minim non magna et ex reprehenderit nulla excepteur commodo qui voluptate cupidatat elit. Sunt enim deserunt amet ipsum enim eiusmod ex culpa cillum esse duis.', '');
+        $service->setPreviewImageUrl('/assets/images/test.gif');
+        $arr []= $service;
+
+        for ($i=1; $i < 15; $i++)
         {
-            $service = new AdditionalServiceDTO($i, "test{$i}", 'Практический опыт показывает, что дальнейшее развитие различных форм деятельности способствует подготовке и реализации новых предложений', '');
+            $service = new AdditionalServiceDTO($i, "ЫВЛАОВДЫЛАОВЫЛДАОЫДЛВ АОЛДЫВОАЛ ДЫВОАДЫЛВОАЛДЫВОАЛД ВЫОАЛД{$i}", 'Практический опыт показывает, что дальнейшее развитие различных форм деятельности способствует подготовке и реализации новых предложений Федя Федя Федя pФедя Федя', '');
             $service->setPreviewImageUrl('/assets/images/test.gif');
             $arr []= $service;
         }
