b9838c22d2e5707b5b66da6b17e52f15f301b024
cekator3 <hamsterdreams@inbox.ru>
2024-04-06 19:55:21 +0500

    writed implementation for printing technologies listing controller

--- app/Http/Controllers/Admin/PrintingTechnologies/PrintingTechnologiesListingController.php
+++ app/Http/Controllers/Admin/PrintingTechnologies/PrintingTechnologiesListingController.php
@@ -3,23 +3,24 @@
 namespace App\Http\Controllers\Admin\PrintingTechnologies;
 
 use Illuminate\View\View;
-use App\DTOs\Admin\PrintingTechnologyDTO;
+use Illuminate\Http\Request;
+use App\Services\Admin\PrintingTechnologies\PrintingTechnologiesGetterService;
 
 class PrintingTechnologiesListingController
 {
-    private function getTestData(int $amount) : array
+    public function showPrintingTechnologies(Request $request) : View
     {
+        $searchQuery = $request->query('search', '');
+        $printingTechnologies = new PrintingTechnologiesGetterService();
+
+        // Apply the search query if exists
         $result = [];
+        if ($searchQuery === '')
+            $result = $printingTechnologies->getAll();
+        else
+            $result = $printingTechnologies->find($searchQuery);
 
-        for ($i = 0; $i < $amount; $i++)
-            $result []= new PrintingTechnologyDTO($i, "test{$i}", 'Практический опыт показывает, что дальнейшее развитие различных форм деятельности способствует подготовке и реализации новых предложений');
-
-        return $result;
-    }
-
-    public function showPrintingTechnologies() : View
-    {
-        // ...
-        return view('admin.printing-technologies.list', ['printingTechnologies' => $this->getTestData(20)]);
+        return view('admin.printing-technologies.list',
+                    ['printingTechnologies' => $result]);
     }
 }
