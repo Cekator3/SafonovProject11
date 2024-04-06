e5db3ee1ce07db0611d52080c7bbea269c448c0b
cekator3 <hamsterdreams@inbox.ru>
2024-04-06 19:58:33 +0500

    implemented controller

--- app/Http/Controllers/Admin/PrintingTechnologies/PrintingTechnologyCreationController.php
+++ app/Http/Controllers/Admin/PrintingTechnologies/PrintingTechnologyCreationController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers\Admin\PrintingTechnologies;
 
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use App\Errors\UserInputErrors;
 use Illuminate\Http\RedirectResponse;
+use App\Services\Admin\PrintingTechnologies\PrintingTechnologiesCreationService;
+use App\ViewModels\Admin\PrintingTechnology\PrintingTechnologyCreationViewModel;
 
 class PrintingTechnologyCreationController
 {
     public function showCreationForm() : View
     {
-        // ...
         return view('admin.printing-technologies.create');
+    }
+
+    private function getUserInput(Request $request) : PrintingTechnologyCreationViewModel
+    {
+        $userInput = new PrintingTechnologyCreationViewModel();
+        $userInput->name = $request->string('name', '');
+        $userInput->description = $request->string('description', '');
+        return $userInput;
     }
 
     /**
@@ -21,6 +31,18 @@ class PrintingTechnologyCreationController
      */
     public function createPrintingTechnology(Request $request) : RedirectResponse
     {
-        // ...
+        $printingTechnology = $this->getUserInput($request);
+        $printingTechnologies = new PrintingTechnologiesCreationService();
+        $errors = new UserInputErrors();
+
+        $printingTechnologies->add($printingTechnology, $errors);
+
+        if ($errors->hasAny()) {
+            return redirect()->back()
+                             ->withErrors($errors->getAll())
+                             ->withInput();
+        }
+
+        return redirect()->route('printing-technologies');
     }
 }
