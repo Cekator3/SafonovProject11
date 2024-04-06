a065df220f486ff8f08511f87590abc0a640f0cf
cekator3 <hamsterdreams@inbox.ru>
2024-04-07 01:18:37 +0500

    created blank views for filament types functionality

---
+++ resources/views/admin/filament-types/list.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title', 'Типы филаментов')
+
+@section('styles')
+{{-- Form --}}
+
+{{-- Specific --}}
+@endsection
+
+@section('navigation')
+<x-navigation.admin />
+@endsection
+
+@section('main')
+@endsection
---
+++ resources/views/admin/filament-types/update.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title', 'Изменение типа филамента')
+
+@section('styles')
+{{-- Form --}}
+
+{{-- Specific --}}
+@endsection
+
+@section('navigation')
+<x-navigation.admin />
+@endsection
+
+@section('main')
+@endsection
