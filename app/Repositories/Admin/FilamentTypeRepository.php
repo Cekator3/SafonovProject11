12941d3264699679bf431d9da9342d0e315a974d
cekator3 <hamsterdreams@inbox.ru>
2024-04-07 20:03:05 +0500

    saving progress

--- app/Repositories/Admin/FilamentTypeRepository.php
+++ app/Repositories/Admin/FilamentTypeRepository.php
@@ -2,7 +2,9 @@
 
 namespace App\Repositories\Admin;
 
+use App\DTOs\Admin\PrintingTechnologies\PrintingTechnologyNameOnlyDTO;
 use stdClass;
+use Illuminate\Support\Facades\DB;
 use App\DTOs\Admin\FilamentTypes\FilamentTypeDTO;
 use App\DTOs\Admin\FilamentTypes\FilamentTypeItemListDTO;
 use App\Errors\Admin\FilamentType\FilamentTypeUpdateErrors;
@@ -15,7 +17,17 @@ use App\ViewModels\Admin\FilamentType\FilamentTypeCreationViewModel;
  */
 class FilamentTypeRepository
 {
-    private const string TABLE_NAME = 'filament_types';
+    /**
+     * @param stdClass $entries
+     */
+    private function getPrintingTechnologies(array $entries) : PrintingTechnologyNameOnlyDTO
+    {
+        foreach ($entries as $entry)
+        {
+            $printingTechnologies = DB::table(static::PRINTING_TECHNOLOGIES_TABLE_NAME)
+                                      ->get(['id', 'name']);
+        }
+    }
 
     /**
      * Returns all filament types
@@ -23,7 +35,14 @@ class FilamentTypeRepository
      */
     public function getAll() : array
     {
-        // ...
+        // 1. Get identifiers of all filament types and associated
+        //    with them printing technologies
+        $entries = DB::table('filament_types', 'ft')
+                     ->join('printing_technologies_of_filament_type AS ptft', 'ptft.filament_type_id', '=', 'ft.id')
+                     ->join('printing_technologies AS pt', 'ptft.printing_technology_id', '=', 'pt.id')
+                     ->select(['ft.id AS filament_type_id', 'pt.id AS printing_technology_id'])
+                     ->get();
+        dd($entries);
     }
 
     /**
