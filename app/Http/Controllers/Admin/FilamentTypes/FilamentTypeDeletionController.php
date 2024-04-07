f63e7210e6fe81eeda7dfd8b9fde6771788b4591
cekator3 <hamsterdreams@inbox.ru>
2024-04-07 22:03:37 +0500

    created implementation for filament type deletion controller

--- app/Http/Controllers/Admin/FilamentTypes/FilamentTypeDeletionController.php
+++ app/Http/Controllers/Admin/FilamentTypes/FilamentTypeDeletionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\FilamentTypes;
 
 use Illuminate\Http\RedirectResponse;
+use App\Services\Admin\FilamentTypes\FilamentTypesDeletionService;
 
 class FilamentTypeDeletionController
 {
@@ -11,7 +12,9 @@ class FilamentTypeDeletionController
      */
     public function deleteFilamentType(int $filamentTypeId) : RedirectResponse
     {
-        // ...
+        $filamentTypes = new FilamentTypesDeletionService();
+        $filamentTypes->remove($filamentTypeId);
+
         return redirect()->route('filament-types');
     }
 }
