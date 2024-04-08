4001aec52660a44b564c03cf657341c14a975d3b
cekator3 <hamsterdreams@inbox.ru>
2024-04-08 14:29:50 +0500

    created colors database table and DTO

---
+++ app/DTOs/Admin/Models/ColorDTO.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DTOs\Models;
+
+/**
+ * A subsystem for reading application data about colors
+ */
+class ColorDTO
+{
+    private int $id;
+    private string $name = '';
+    private string $code = '';
+
+    public function __construct(int $id,
+                                string $name,
+                                string $code)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->code = $code;
+    }
+
+    /**
+     * Returns the id of the color
+     */
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Returns the name of the color
+     */
+    public function getName() : string
+    {
+        assert($this->name !== '', 'accessing not initialized property: $name');
+        return $this->name;
+    }
+
+    /**
+     * Returns the color's rgb code
+     */
+    public function getRgb() : string
+    {
+        assert($this->code !== '', 'accessing not initialized property: $code');
+        return $this->code;
+    }
+}
