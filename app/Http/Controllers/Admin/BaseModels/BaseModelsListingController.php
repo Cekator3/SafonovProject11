b0f55786494df9c9d6bc1ff4e16109208568b565
cekator3 <hamsterdreams@inbox.ru>
2024-04-09 12:25:45 +0500

    created view for base models listing

--- app/Http/Controllers/Admin/BaseModels/BaseModelsListingController.php
+++ app/Http/Controllers/Admin/BaseModels/BaseModelsListingController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers\Admin\BaseModels;
 
+use App\DTOs\Admin\BaseModels\ModelItemListDTO;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class BaseModelsListingController
 {
+    private function getTestData(int $amount) : array
+    {
+        $result = [];
+
+        for ($i = 0; $i < $amount; $i++)
+        {
+            $model = new ModelItemListDTO($i, "test{$i}", '');
+            $model->setPreviewImageUrl('/assets/images/test.gif');
+            $result []= $model;
+        }
+
+        return $result;
+    }
+
     public function showBaseModels(Request $request) : View
     {
         // ...
-        return view('admin.base-models.list');
+        return view('admin.base-models.list', ['baseModels' => $this->getTestData(20)]);
     }
 }
--- resources/views/admin/base-models/list.blade.php
+++ resources/views/admin/base-models/list.blade.php
@@ -4,8 +4,13 @@
 
 @section('styles')
 {{-- Form --}}
+<link href="/assets/css/form/common.css" rel="stylesheet" type="text/css">
+<link href="/assets/css/form/text.css" rel="stylesheet" type="text/css">
+<link href="/assets/css/form/submit.css" rel="stylesheet" type="text/css">
+<link href="/assets/css/search.css" rel="stylesheet" type="text/css">
 
 {{-- Specific --}}
+<link href="/assets/css/admin/base-models/list.css" rel="stylesheet" type="text/css">
 @endsection
 
 @section('navigation')
@@ -32,7 +37,27 @@
 @foreach ($baseModels as $baseModel)
 <li>
     <section class="base-model">
-        {{-- ... --}}
+        {{-- Title and description --}}
+        <a href="{{ route('base-models.update', ['id' => $baseModel->getId()]) }}">
+            {{-- Preview image and title --}}
+            <img loading="lazy" src="{{ $baseModel->getPreviewImageUrl() }}">
+            <header>
+                <h3>{{ $baseModel->getName() }}</h3>
+            </header>
+        </a>
+
+        {{-- Update and delete buttons --}}
+        <footer class="actions">
+            <form method="GET" action="{{ route('base-models.update', ['id' => $baseModel->getId()]) }}">
+                <x-forms.submit :placeholder=" 'Изменить' " />
+            </form>
+
+            <form method="POST" action="{{ route('base-models.delete', ['id' => $baseModel->getId()]) }}">
+                @csrf
+                @method('DELETE')
+                <x-forms.submit :placeholder=" 'Удалить' " />
+            </form>
+        </footer>
     </section>
 </li>
 @endforeach
