688038e34ccc3f1c20c62c11bef825f85fb7c11b
cekator3 <hamsterdreams@inbox.ru>
2024-04-10 16:57:03 +0500

    created interface for module

---
+++ public/assets/js/admin/base-models/gallery-image.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * Marks a gallery image for deletion.
+ *
+ * @param {HTMLLIElement} image
+ * @returns {void}
+ */
+export function GalleryImageDelete(image)
+{
+
+}
+
+/**
+ * Removes deletion mark from the gallery image.
+ *
+ * @param {HTMLLIElement} image
+ * @returns {void}
+ */
+export function GalleryImageRestore(image)
+{
+
+}
+
+/**
+ * Returns identifiers of all marked for deletion images.
+ *
+ * @returns {number[]}
+ */
+export function GalleryImageGetDeleted()
+{
+    
+}
