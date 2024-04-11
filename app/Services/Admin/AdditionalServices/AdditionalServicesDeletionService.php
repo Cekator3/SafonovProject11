7adda712f5852df90175f3ce850c6ea98102324a
cekator3 <hamsterdreams@inbox.ru>
2024-04-11 22:28:53 +0500

    bug fix

--- app/Services/Admin/AdditionalServices/AdditionalServicesDeletionService.php
+++ app/Services/Admin/AdditionalServices/AdditionalServicesDeletionService.php
@@ -3,18 +3,30 @@
 namespace App\Services\Admin\AdditionalServices;
 
 use App\Repositories\Admin\AdditionalServiceRepository;
+use App\Repositories\Images\AdditionalServiceThumbnailRepository;
 
 /**
  * Subsystem for deleting stored information on additional service.
  */
 class AdditionalServicesDeletionService
 {
+    private function removeThumbnail(string $filename) : void
+    {
+        $thumbnails = new AdditionalServiceThumbnailRepository();
+        $thumbnails->remove($filename);
+    }
+
     /**
      * Deletes the additional service.
+     *
+     * @param int $id The identifier of the additional service
      */
-    public function remove(int $additionalServiceId) : void
+    public function remove(int $id) : void
     {
         $additionalServices = new AdditionalServiceRepository();
-        $additionalServices->remove($additionalServiceId);
+
+        $thumbnail = $additionalServices->getThumbnail($id);
+        $additionalServices->remove($id);
+        $this->removeThumbnail($thumbnail);
     }
 }
