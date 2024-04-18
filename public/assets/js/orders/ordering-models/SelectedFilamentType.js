3e5a6ab32926b91046a540a1c52193afe3135d3e
cekator3 <hamsterdreams@inbox.ru>
2024-04-18 22:47:00 +0500

    saving progress

---
+++ public/assets/js/orders/ordering-models/SelectedFilamentType.js
@@ -0,0 +1,34 @@
+/**
+ * @file A subsystem for interaction with the user-selected filament type
+ */
+
+'use strict';
+
+/**
+ * Returns input of filament type DOM element
+ * @param {HTMLLIElement} filamentType
+ * @returns {HTMLInputElement}
+ */
+function getInput(filamentType)
+{
+
+}
+
+/**
+ * Retrieves identifier of filament type that is selected by user
+ * @returns {number}
+ */
+export function SelectedFilamentTypeGet()
+{
+    // ...
+}
+
+/**
+ * Unselects a filament type if it is selected by user.
+ * @param {number} id
+ * @returns {void}
+ */
+export function SelectedFilamentTypeUnselect(id)
+{
+    // ...
+}
---
+++ public/assets/js/orders/ordering-models/VisibleFilamentTypes.js
@@ -0,0 +1,15 @@
+/**
+ * @file A subsystem for hiding filament types.
+ */
+
+'use strict';
+
+/**
+ * Sets filament types that will be visible to the user. Others will be hidden.
+ * @param {number[]} ids
+ * @returns {void}
+ */
+export function VisibleFilamentTypesSet(ids)
+{
+    // ...
+}
