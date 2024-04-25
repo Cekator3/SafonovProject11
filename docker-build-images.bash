0427031bd3807ffbb1d3a61a403a27921679c232
cekator3 <hamsterdreams@inbox.ru>
2024-04-25 12:33:14 +0500

    created script for building docker images

---
+++ docker-build-images.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Sets script's behavior: exit on first error
+set -e
+
+docker-compose down
+docker build -t php_img -f .docker/php/Dockerfile .
+docker build -t nginx_img -f .docker/nginx/Dockerfile ./
+docker-compose up -d --build
+
+# Run migrations
+printf 'waiting for docker services to up'
+sleep 3s
+docker-compose exec php php artisan migrate --seed
+# Changin owner of logs file (from root to www-data)
+docker-compose exec php bash -c "chown www-data:www-data /var/www/html/storage/logs/laravel.log"
