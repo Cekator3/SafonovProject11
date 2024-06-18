c4210d3d0a0084cc88c3e8bb00f91df29e7fbeff
cekator3 <hamsterdreams@inbox.ru>
2024-06-18 14:40:55 +0500

    updated order payment page

--- app/Http/Controllers/Orders/OrderPaymentController.php
+++ app/Http/Controllers/Orders/OrderPaymentController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers\Orders;
 
-use App\Enums\HttpResponseStatus;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Repositories\Orders\OrderRepository;
+use App\Services\Orders\OrderedModels\OrderedModelGetterService;
 
 class OrderPaymentController
 {
@@ -15,13 +13,8 @@ class OrderPaymentController
      */
     public function showPaymentNotifier(Request $request) : View
     {
-        $userId = Auth::user()->id;
-        $orders = new OrderRepository();
-        $orderId = $orders->getCurrentOrderId($userId);
-
-        if ($orderId === null)
-            abort(HttpResponseStatus::NotFound->value);
-
-        return view('orders.payment', ['orderId' => $orderId]);
+        $models = new OrderedModelGetterService();
+        $models = $models->getAllAsShoppingCart();
+        return view('orders.payment', ['shoppingCart' => $models]);
     }
 }
